54a167a01480747b6c08552e2fe7158a1bb4256f
kalumuclaire <clairemutisya97@gmail.com>
2023-07-16 12:37:46 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: 1
+ * main - A c program that prints a line to thee standard error
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that place of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fputs("and that place of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
